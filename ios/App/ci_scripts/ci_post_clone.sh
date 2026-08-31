bb1877cb4020486c4437b015bb4a983c8cab8fbb
TomLiam <tom.liam@gmail.com>
2026-08-31 16:53:31 -0400

    Fix ci_post_clone.sh: run npm install before pod install

--- ios/App/ci_scripts/ci_post_clone.sh
+++ ios/App/ci_scripts/ci_post_clone.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 set -e
+
+echo "Installing npm dependencies..."
+cd "$CI_PRIMARY_REPOSITORY_PATH"
+npm install
+
 echo "Installing CocoaPods dependencies..."
 cd "$CI_PRIMARY_REPOSITORY_PATH/ios/App"
 pod install
